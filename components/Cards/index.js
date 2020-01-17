faa64ef86f667e5441fa6b708d5a39f4316ec2f4
TREY <hugh.a.lloyd@gmail.com>
2020-01-17 11:56:13 -0600

    someProgOn3

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,33 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    console.log(response.data);
+})
+.catch(error => {
+    console.log('data not returned', error)
+  })
+
+  function articleComp(){
+      const cardDivParent = document.createElement('div');
+      headlineDiv = document.createElement('div');
+      authorDiv = document.createElement('div');
+      imgDiv = document.createElement('div');
+      imgUrl = document.createElement('img');
+      authorNameSpan = createImageBitmap('span');
+
+      cardDivParent.classList.add('card');
+      headlineDiv.classList.add('headline');
+      authorDiv.classList.add('author');
+      imgDiv.classList.add('img-container');
+
+        cardDivParent.append(headlineDiv);
+        cardDivParent.append(authorDiv);
+        authorDiv.append(imgDiv);
+        authorDiv.append(authorNameSpan)
+        imgDiv.append(imgUrl);
+
+
+  }
