8971ab0a5b416867aa67cbf8c2eec4466f07c206
TREY <hugh.a.lloyd@gmail.com>
2020-01-17 11:25:37 -0600

    1&2compl

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,5 +11,25 @@
 
 function Header() {
 const compParentHeader = document.createElement('div');
+compDateSpan = document.createElement('span');
+compH1 = document.createElement('h1');
+compTempSpan = document.createElement('span');
 
+
+compParentHeader.classList.add('header');
+compDateSpan.classList.add('date');
+compTempSpan.classList.add('temp');
+
+compParentHeader.append(compDateSpan);
+compParentHeader.append(compH1);
+compParentHeader.append(compTempSpan);
+
+compDateSpan.textContent = 'MARCH 28, 2019';
+compH1.textContent = 'Lambda Times';
+compTempSpan.textContent = '98 degrees';
+
+return compParentHeader;
 }
+
+const htmlHeaderContainer = document.querySelector('.header-container');
+htmlHeaderContainer.append(Header());
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,22 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+  
+
+function tabComp(apiParam){
+const topicTabParent = document.createElement('div');
+topicTabParent.classList.add('tab');
+ topicTabParent.textContent = apiParam.topics;
+ return topicTabParent;
+}
+const parentTopics = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response =>{
+    console.log(response.data);
+    const compData = tabComp(response.data)
+    parentTopics.append(compData);
+})
+.catch(error => {
+    console.log('data not returned', error)
+  })
